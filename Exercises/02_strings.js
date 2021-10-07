088e8661f2c273c1679c990d62c003b10c271646
´Gabriela <gabrielaminni@gmail.com>
2021-10-07 14:18:40 -0300

    2.b

--- Exercises/02_strings.js
+++ Exercises/02_strings.js
@@ -1,8 +1,13 @@
-//a. Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar toUpperCase). //
+/*a. Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar toUpperCase). */
 
 var message1 = "Hola mundo ¿Cómo estas?";
 var message2 = message1.toUpperCase();
 
 console.log(message2)
 
+/* b Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).*/
 
+var fruits = "limon, pera y mandarina";
+var fruits1 = fruits.substring (0,5);
+
+console.log(fruits1)
