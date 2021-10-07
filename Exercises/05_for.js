b1543574a7ea468d6872467a4f9b5d9e6c40d8be
´Gabriela <gabrielaminni@gmail.com>
2021-10-07 17:37:00 -0300

    5.a

---
+++ Exercises/05_for.js
@@ -0,0 +1,7 @@
+/* a. Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de JavaScript para mostrar una alerta utilizando cada una de las palabras.*/
+ 
+var palabras = ["nombre", "apellido", "sexo", "edad", "nacionalidad"]
+for (let i = 0; i < palabras.length; i++) {
+    alert(palabras[i]);
+}
+
