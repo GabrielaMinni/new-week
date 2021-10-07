a48acdfbe30c4d9c5eecc091f8f1d6c13926f15a
´Gabriela <gabrielaminni@gmail.com>
2021-10-07 15:45:26 -0300

    ¨3.c¨

--- Exercises/03_arrays.js
+++ Exercises/03_arrays.js
@@ -4,8 +4,13 @@ var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Ag
  
 console.log(meses[5], meses[11]);
 
-// b. Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort). //
+// b. Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort). 
 
 console.log(meses.sort());
- 
 
+// c. Agregar un elemento al principio y al final del array (utilizar unshift y push).
+
+meses.unshift("verano")
+meses.push("otoño")
+
+console.log(meses)
