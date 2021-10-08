9da9bd08a2dd071df8909292e95059cb78daa094
´Gabriela <gabrielaminni@gmail.com>
2021-10-08 15:33:38 -0300

    6.c

--- Exercises/06_functions.js
+++ Exercises/06_functions.js
@@ -1,14 +1,14 @@
 /* a. Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.*/
-
+/*
 function suma (a, b){
     var resultado = a+b;
     console.log(resultado);
 }
 
 suma (4,3);
-
+*/
 /*b. A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros no es un número, mostrar una alerta aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado.*/
-
+/*
 function adicion(a,b) {
     if (isNaN(a) || isNaN(b)){
       console.log('NaN');
@@ -19,3 +19,10 @@ function adicion(a,b) {
 }    
 
 adicion (5,"hola")
+*/
+/* c. Crear una función validate integer que reciba un número como parámetro y devuelva verdadero si es un número entero. */
+
+function validation(x) {
+    console.log(Number.isInteger(x))
+}      
+    validation (5) 
